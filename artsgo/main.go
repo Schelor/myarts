b46140fdf4f16e62fbb524c657f3100851888cc1
xiele02 <Schelor@users.noreply.github.com>
2023-09-06 13:59:54 +0800

    add arts-4 algo

--- artsgo/main.go
+++ artsgo/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"artsgo/arts3"
+	"artsgo/arts4"
 	"fmt"
 )
 
@@ -9,6 +9,7 @@ func main() {
 	// fmt.Println("Hello,Arts")
 	// arts1.AddTwoNumbers(&arts1.ListNode{Val: 100}, &arts1.ListNode{Val: 1})
 	// ans := arts2.LengthOfLongestSubstring("abcabcbb")
-	fmt.Println(arts3.FindMedianSortedArrays([]int{1, 2}, []int{3, 4}))
+	//fmt.Println(arts3.FindMedianSortedArrays([]int{1, 2}, []int{3, 4}))
+	fmt.Println(arts4.LongestPalindrome3("babad"))
 
 }
